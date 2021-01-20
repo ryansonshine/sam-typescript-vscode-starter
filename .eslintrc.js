5476afca57f8376b618a988f19e965c7ace79620
Ryan Sonshine <ryansonshine@users.noreply.github.com>
2021-01-20 15:10:23 +0700

    Set unused vars linting rule to warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     'node/shebang': 'off',
     quotes: ['warn', 'single', { avoidEscape: true }],
     'node/no-unpublished-import': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
   overrides: [
     {
